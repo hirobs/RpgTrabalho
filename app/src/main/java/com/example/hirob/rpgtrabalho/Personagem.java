ab4934770e25d481127e7af16b986bd4feb36612
hirobs <hiro.bs@outlook.com>
2017-11-02 20:23:25 -0200

    Adicionei classe personagem

---
+++ app/src/main/java/com/example/hirob/rpgtrabalho/Personagem.java
@@ -0,0 +1,88 @@
+package com.example.hirob.rpgtrabalho;
+
+/**
+ * Created by hirob on 02/11/2017.
+ */
+
+public class Personagem {
+    private String nmPersonagem;
+    private int atkMax;
+    private int  atkMin;
+    private int defense;
+    private int gold;
+    private boolean jogavel;
+    private String itemUm;
+    private String itemDois;
+
+    public String getNmPersonagem() {
+        return nmPersonagem;
+    }
+
+    public void setNmPersonagem(String nmPersonagem) {
+        this.nmPersonagem = nmPersonagem;
+    }
+
+    public int getAtkMax() {
+        return atkMax;
+    }
+
+    public void setAtkMax(int atkMax) {
+        this.atkMax = atkMax;
+    }
+
+    public int getAtkMin() {
+        return atkMin;
+    }
+
+    public void setAtkMin(int atkMin) {
+        this.atkMin = atkMin;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public boolean isJogavel() {
+        return jogavel;
+    }
+
+    public void setJogavel(boolean jogavel) {
+        this.jogavel = jogavel;
+    }
+
+    public String getItemUm() {
+        return itemUm;
+    }
+
+    public void setItemUm(String itemUm) {
+        this.itemUm = itemUm;
+    }
+
+    public String getItemDois() {
+        return itemDois;
+    }
+
+    public void setItemDois(String itemDois) {
+        this.itemDois = itemDois;
+    }
+
+    public int intJogavel(){
+        if(jogavel){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
